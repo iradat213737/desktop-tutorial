3fc51b6ab47a3be6d90e8a1560e36923800e3042
maria <f190908@nu.edu.pk>
2022-04-14 00:03:25 +0500

    maria1

---
+++ db/conn.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/maria",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection succesfull");
+}).catch((e)=>{
+    console.log(e);
+})
